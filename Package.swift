7640f63db725a04360a413765ecba9ed9dd8b31e
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-03-13 22:01:52 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,55 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "swift-emailaddress",
+    platforms: [
+        .macOS(.v26),
+        .iOS(.v26),
+        .tvOS(.v26),
+        .watchOS(.v26),
+        .visionOS(.v26)
+    ],
+    products: [
+        .library(
+            name: "EmailAddress",
+            targets: ["EmailAddress"]
+        )
+    ],
+    dependencies: [
+        .package(path: "../../swift-standards/swift-emailaddress-standard"),
+    ],
+    targets: [
+        .target(
+            name: "EmailAddress",
+            dependencies: [
+                .product(name: "EmailAddress Standard", package: "swift-emailaddress-standard"),
+            ]
+        ),
+        .testTarget(
+            name: "EmailAddress Tests",
+            dependencies: [
+                "EmailAddress",
+            ]
+        ),
+    ],
+    swiftLanguageModes: [.v6]
+)
+
+for target in package.targets where ![.system, .binary, .plugin, .macro].contains(target.type) {
+    let ecosystem: [SwiftSetting] = [
+        .strictMemorySafety(),
+        .enableUpcomingFeature("ExistentialAny"),
+        .enableUpcomingFeature("InternalImportsByDefault"),
+        .enableUpcomingFeature("MemberImportVisibility"),
+        .enableUpcomingFeature("NonisolatedNonsendingByDefault"),
+        .enableExperimentalFeature("Lifetimes"),
+        .enableExperimentalFeature("SuppressedAssociatedTypes"),
+        .enableExperimentalFeature("SuppressedAssociatedTypesWithDefaults"),
+    ]
+
+    let package: [SwiftSetting] = []
+
+    target.swiftSettings = (target.swiftSettings ?? []) + ecosystem + package
+}
---
+++ Tests/EmailAddress Tests/EmailAddress Tests.swift
@@ -0,0 +1,20 @@
+// ===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-emailaddress open source project
+//
+// Copyright (c) 2025 Coen ten Thije Boonkkamp and the swift-emailaddress project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// ===----------------------------------------------------------------------===//
+
+import Testing
+import EmailAddress
+
+@Suite
+struct EmailAddressTests {
+    @Test
+    func `EmailAddress module re-exports EmailAddress_Standard`() {
+    }
+}
