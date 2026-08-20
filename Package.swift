bbdb1a24fb8b1b37b804334fb50847619b66cf61
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:45:52 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-emailaddress",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,19 +18,22 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-standards/swift-emailaddress-standard.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-standards/swift-emailaddress-standard.git",
+            branch: "main"
+        )
     ],
     targets: [
         .target(
             name: "EmailAddress",
             dependencies: [
-                .product(name: "EmailAddress Standard", package: "swift-emailaddress-standard"),
+                .product(name: "EmailAddress Standard", package: "swift-emailaddress-standard")
             ]
         ),
         .testTarget(
             name: "EmailAddress Tests",
             dependencies: [
-                "EmailAddress",
+                "EmailAddress"
             ]
         ),
     ],
