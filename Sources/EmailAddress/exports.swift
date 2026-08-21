2b1c204ec1db4041d30a6db68ae2df3df8f14a79
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 15:50:44 +0200

    Remove source comments and DocC

--- Sources/EmailAddress/exports.swift
+++ Sources/EmailAddress/exports.swift
@@ -1,12 +1,1 @@
-// ===----------------------------------------------------------------------===//
-//
-// This source file is part of the swift-emailaddress open source project
-//
-// Copyright (c) 2025 Coen ten Thije Boonkkamp and the swift-emailaddress project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE for license information
-//
-// ===----------------------------------------------------------------------===//
-
 @_exported public import EmailAddress_Standard
--- Tests/EmailAddress Tests/EmailAddress Tests.swift
+++ Tests/EmailAddress Tests/EmailAddress Tests.swift
@@ -1,14 +1,3 @@
-// ===----------------------------------------------------------------------===//
-//
-// This source file is part of the swift-emailaddress open source project
-//
-// Copyright (c) 2025 Coen ten Thije Boonkkamp and the swift-emailaddress project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE for license information
-//
-// ===----------------------------------------------------------------------===//
-
 import EmailAddress
 import Testing
 
